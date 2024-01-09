236a1780112d01b55e049e95227e1e3d88ee27ce
amanySaad <eng.am.aboelella@gmail.com>
2024-01-08 22:38:47 -0800

    update

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
 import string
-
-# Get all uppercase letters from string module
-uppercase_letters = string.ascii_uppercase
-
-# Print all uppercase letters
-print(uppercase_letters)
+print(string.ascii_uppercase)
